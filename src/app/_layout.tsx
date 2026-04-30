a1f87f934f69b99f0a2d51369c223a9dcf7dc56c
vikrantaroraa <arora.veshu@gmail.com>
2026-04-30 20:12:51 +0530

    fix: fixed status bar noot showing icons

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,15 +1,19 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{ 
-      headerShadowVisible: false, 
-      headerStyle: { backgroundColor: '#F9FAFB' }, 
-      contentStyle: { backgroundColor: '#F9FAFB' } 
-    }}>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
-      <Stack.Screen name="calendar" options={{ title: "Calendar", headerBackVisible: true, headerBackTitleVisible: false }} />
-      <Stack.Screen name="journal/[date]" options={{ title: "Journal", headerBackVisible: true, headerBackTitleVisible: false }} />
-    </Stack>
+    <>
+      <StatusBar style="dark" />
+      <Stack screenOptions={{ 
+        headerShadowVisible: false, 
+        headerStyle: { backgroundColor: '#F9FAFB' }, 
+        contentStyle: { backgroundColor: '#F9FAFB' } 
+      }}>
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+        <Stack.Screen name="calendar" options={{ title: "Calendar", headerBackVisible: true, headerBackTitleVisible: false }} />
+        <Stack.Screen name="journal/[date]" options={{ title: "Journal", headerBackVisible: true, headerBackTitleVisible: false }} />
+      </Stack>
+    </>
   );
 }
